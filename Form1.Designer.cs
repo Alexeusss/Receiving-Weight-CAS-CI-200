2241a66e48aeaacdcc230c7a2176602e8ef3f466
РусаковА <@RUSAKOVA>
2024-06-26 11:10:59 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ComPortReader
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comPortComboBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.CheckBox alwaysOnTopCheckBox;
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comPortComboBox = new System.Windows.Forms.ComboBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.alwaysOnTopCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comPortComboBox
+            // 
+            this.comPortComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPortComboBox.FormattingEnabled = true;
+            this.comPortComboBox.Location = new System.Drawing.Point(10, 10);
+            this.comPortComboBox.Name = "comPortComboBox";
+            this.comPortComboBox.Size = new System.Drawing.Size(360, 21);
+            this.comPortComboBox.TabIndex = 0;
+            this.comPortComboBox.SelectedIndexChanged += new System.EventHandler(this.ComPortComboBox_SelectedIndexChanged);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Black;
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(10, 40);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(360, 70);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // alwaysOnTopCheckBox
+            // 
+            this.alwaysOnTopCheckBox.AutoSize = true;
+            this.alwaysOnTopCheckBox.Checked = true;
+            this.alwaysOnTopCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.alwaysOnTopCheckBox.ForeColor = System.Drawing.Color.Black;
+            this.alwaysOnTopCheckBox.Location = new System.Drawing.Point(10, 120);
+            this.alwaysOnTopCheckBox.Name = "alwaysOnTopCheckBox";
+            this.alwaysOnTopCheckBox.Size = new System.Drawing.Size(116, 17);
+            this.alwaysOnTopCheckBox.TabIndex = 2;
+            this.alwaysOnTopCheckBox.Text = "Поверх всех окон";
+            this.alwaysOnTopCheckBox.UseVisualStyleBackColor = true;
+            this.alwaysOnTopCheckBox.CheckedChanged += new System.EventHandler(this.alwaysOnTopCheckBox_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Автор: Алексей Русаков";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Для свободного распространения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(384, 150);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comPortComboBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.alwaysOnTopCheckBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Весовой терминал CAS CI-200";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ComPortReader
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private const int maxBufferLength = 100; // Максимальный размер буфера данных
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailablePorts();
+        }
+
+        private void LoadAvailablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comPortComboBox.Items.AddRange(ports);
+            if (ports.Length > 0)
+            {
+                comPortComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void ComPortComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                serialPort.Dispose();
+            }
+
+            string selectedPort = comPortComboBox.SelectedItem.ToString();
+            serialPort = new SerialPort(selectedPort, 9600)
+            {
+                ReadTimeout = 1000
+            };
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                outputLabel.ForeColor = Color.White;
+                outputLabel.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort.ReadLine();
+                this.Invoke(new Action(() => ProcessData(data)));
+            }
+            catch (TimeoutException) { }
+        }
+
+        private void ProcessData(string data)
+        {
+            if (data.Length > maxBufferLength)
+            {
+                data = data.Substring(0, maxBufferLength);
+            }
+
+            Regex weightPattern = new Regex(@"(\d+\.\d+)\s*kg");
+            Match match = weightPattern.Match(data);
+
+            if (match.Success)
+            {
+                string weight = match.Groups[1].Value;
+                if (data.Contains("ST"))
+                {
+                    outputLabel.ForeColor = Color.Green;
+                    outputLabel.Text = "СТАБ: " + weight + "0 kg";
+                }
+                else if (data.Contains("US"))
+                {
+                    outputLabel.ForeColor = Color.Red;
+                    outputLabel.Text = "НСТАБ: " + weight + "0 kg";
+                }
+                else
+                {
+                    outputLabel.ForeColor = Color.White;
+                    outputLabel.Text = "Received: " + data;
+                }
+            }
+            else
+            {
+                outputLabel.ForeColor = Color.White;
+                outputLabel.Text = "Received: " + data;
+            }
+
+            // Принудительная сборка мусора
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+        private void alwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = alwaysOnTopCheckBox.Checked;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
